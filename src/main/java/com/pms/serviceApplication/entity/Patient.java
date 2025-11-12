f4df2e7cd865d171dde93a1afa44ecec949e2894
arjun <arju.ac591@gmail.com>
2025-11-12 11:28:52 +0530

    entity class updated

--- src/main/java/com/pms/serviceApplication/entity/Patient.java
+++ src/main/java/com/pms/serviceApplication/entity/Patient.java
@@ -1,8 +1,20 @@
 package com.pms.serviceApplication.entity;
 
 import jakarta.persistence.Entity;
+import jdk.jfr.DataAmount;
+
+import java.sql.Date;
+
 
 @Entity
+
 public class Patient {
+
+    private String name;
+    private String email;
+    private String address;
+    private Date dob;
+    private Date registeredDate;
+
 
 }
